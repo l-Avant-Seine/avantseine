8223821f95769381b333dfb8720226b8d757b496
thomas <hello@thomasflorentin.net>
2017-05-09 15:11:59 +0200

    Add front page template

---
+++ wp-content/themes/lavantseine-v2/template-parts/contents/content-home.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying page content in page-home.php
+ *
+ * @package l\'Avant-Seine_v2.0
+ */
+
+?>
+
+
+<div>
+	
+
+	<!-- Focus -->
+	
+	<div id="" class="module-focus">
+		Focus
+
+	</div>
+
+
+
+	<!-- Spectacle à venir -->
+
+	<div id="" class="module-nextEvents">
+		Les prochains événements
+
+	</div>
+
+
+	<!-- Pages -->
+	<div id="" class="module-somePages">
+		pages
+
+	</div>
+
+
+
+	<!-- Derniers articles du magazine -->
+	<div id="" class="module-lastArticles">
+		derniers articles
+
+	</div>
+
+
+</div>
