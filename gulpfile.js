bbdb5df2edd0deced4f7bf9e64cbac455afe3473
thomas <hello@thomasflorentin.net>
2017-05-09 14:20:56 +0200

    Add gulpfile and params

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var notify = require('gulp-notify');
+
+// Set up project
+var app_folder = 'wp-content/themes/lavantseine-v2/';
+var public_folder = 'wp-content/themes/lavantseine-v2/';
+var theme_folder = 'wp-content/themes/lavantseine-v2/';
+var assets_folder = 'wp-content/themes/lavantseine-v2/assets/';
+
+var jsfolder = assets_folder + 'js/';
+var mainjs = assets_folder + 'js/scripts.js';
+var vendorsjs = assets_folder + 'js/lib/*.js';
+var alljs = [vendorsjs, mainjs];
+
+var scssfiles = assets_folder + 'assets/css/**/*.scss';
+var sassfolder = assets_folder + 'assets/css/';
+var sassMain = sassfolder + 'style.scss';
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(mainjs)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('styles', function() {
+    return sass(sassMain, { style: 'nested', sourcemap: true })
+    .on('error', sass.logError)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(theme_folder))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest(theme_folder))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(alljs)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(jsfolder))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsfolder))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch(mainjs, ['lint', 'scripts']);
+    gulp.watch(scssfiles, ['styles']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);
+
