623cabb9123c78cb1cb193415e2113ce503207cb
thomas <hello@thomasflorentin.net>
2017-05-12 13:52:45 +0200

    grid

--- wp-content/themes/lavantseine-v2/template-parts/modules/module-focus.php
+++ wp-content/themes/lavantseine-v2/template-parts/modules/module-focus.php
@@ -3,8 +3,9 @@
 
 
 <section class="module-focus">
-	<div class="moduleInner">
+	<div class="moduleInner row">
 		
+
 		<?php 
 			$focus_event = get_field('focus_event', 'option'); 
  
@@ -12,7 +13,7 @@
 				$post = $focus_event;
 				setup_postdata( $post ); ?>
 
-	    <div class="">
+	    <div class="m-4col">
 	    	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 	    </div>
     	<?php wp_reset_postdata();  ?>
@@ -20,21 +21,23 @@
 
 
 
-		<?php if( have_rows('focus_elements', 'option') ):
+		<?php if( have_rows('focus_elements', 'option') ): ?>
+			<div class="m-4col">
+					<div class="row">
 
-		    while ( have_rows('focus_elements', 'option') ) : the_row();
+		   <?php while ( have_rows('focus_elements', 'option') ) : the_row();
 
 		        if( get_row_layout() == 'focusElements_page' ):
 		        	$focusElements_page = get_sub_field('focusElements_page'); ?>
 
-							<div class="">
+							<div class="m-1col">
 					    	<h3><a href="<?php echo get_permalink( $focusElements_page->ID ); ?>"><?php echo $focusElements_page->post_title; ?></a></h3>
 					    </div>
 
 		        <?php elseif( get_row_layout() == 'focusElements_article' ): 
 		        	$focusElements_article = get_sub_field('focusElements_article'); ?>
 
-							<div class="">
+							<div class="m-1col">
 					    	<h3><a href="<?php echo get_permalink( $focusElements_article->ID ); ?>"><?php echo $focusElements_article->post_title; ?></a></h3>
 					    </div>
 
@@ -42,16 +45,18 @@
 		        <?php elseif( get_row_layout() == 'focusElements_libre' ): 
 		        	$focusElements_libre = get_sub_field('focusElements_libre'); ?>
 							
-							<div class="">
+							<div class="m-1col">
 		        		<?php echo $focusElements_libre; ?>
 							</div>
 
 
 		        <?php endif;
 
-		    endwhile;
+		    endwhile; ?>
+		    	</div>
+				</div>
 
-		else :
+		<?php else :
 
 		endif; ?>
 
