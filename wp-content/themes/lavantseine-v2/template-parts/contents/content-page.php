163a4bea8b3c8ca27f7d32ee1c16c5e135ac4395
thomas <hello@thomasflorentin.net>
2017-05-23 15:27:21 +0200

    Add childpages loop

--- wp-content/themes/lavantseine-v2/template-parts/contents/content-page.php
+++ wp-content/themes/lavantseine-v2/template-parts/contents/content-page.php
@@ -7,13 +7,37 @@
  * @package l\'Avant-Seine_v2.0
  */
 
+	$ancestors = get_post_ancestors($post);
+	$level = count($ancestors);
+	$ariane = '';
+
+	if( $level == 0 ) {
+		$root = get_the_ID();
+		$children = get_page_children($root, $pages);
+		$root_title = get_the_title($root);
+		$root_title_url = get_permalink($root);	
+	} 
+	else {
+		$root = end($ancestors);
+		$root_title = get_the_title($root);
+		$root_title_url = get_permalink($root);	
+	}
+
+	$page_intro = get_field( 'pageDetail_intro' ); 
+	$page_right_col = get_field( 'pageDetail_rightCol' );
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php $page_intro = get_field( 'pageDetail_intro' ); ?>
-	<?php $page_right_col = get_field( 'pageDetail_rightCol' ); ?>
 
+	<h1><?php echo $root_title; ?></h1>
+
+	<div class="page-nav">
+		
+		<?php set_query_var( 'root', $root ); ?>
+		<?php get_template_part('template-parts/loops/loop', 'childpages'); ?>
+
+	</div>
 
 	<div class="wrap entry-media">
 		<?php the_post_thumbnail(''); ?>
---
+++ wp-content/themes/lavantseine-v2/template-parts/loops/loop-childpages.php
@@ -0,0 +1,24 @@
+<?php 
+
+global $wp;
+$current_url = home_url(add_query_arg(array(),$wp->request)) . '/';
+
+$childpages = new WP_Query( array(
+  'post_type'      => 'page', 
+  'post_parent'    => $root,
+  'posts_per_page' => -1,
+  'orderby'        => 'menu_order'
+)); ?>
+
+
+  <ul class="pages-menu no-bullet">
+
+    <?php while ( $childpages->have_posts() ) : $childpages->the_post(); ?>
+
+      <li class="pages-menu-item <?php if( strpos($current_url, get_permalink() ) !== false ) { echo 'active'; } ?>">
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      </li>
+
+    <?php endwhile; wp_reset_query(); ?>  
+
+  </ul>    
