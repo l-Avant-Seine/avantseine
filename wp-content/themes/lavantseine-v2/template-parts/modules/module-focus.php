f6810bfa7825efe6af6205de9aaff582cb08fded
thomas <hello@thomasflorentin.net>
2017-05-24 14:30:55 +0200

    First Polish Focus

--- wp-content/themes/lavantseine-v2/template-parts/modules/module-focus.php
+++ wp-content/themes/lavantseine-v2/template-parts/modules/module-focus.php
@@ -1,51 +1,60 @@
 
 
+<?php
+	$focus_event = get_field('focus_event', 'option'); 
+	$focus_event_id = $focus_event->ID;
+	$focus_event_media_url = wp_get_attachment_image_src( get_post_thumbnail_id( $focus_event_id ), 'large' );
 
 
-<section class="module-focus">
-	<div class="moduleInner row">
-		
+	if( $focus_event ): ?>
 
-		<?php 
-			$focus_event = get_field('focus_event', 'option'); 
- 
-			if( $focus_event ): 
-				$post = $focus_event;
-				setup_postdata( $post ); ?>
 
-	    <div class="m-4col">
-	    	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-	    </div>
-    	<?php wp_reset_postdata();  ?>
-		<?php endif; ?>
+	<section class="module-focus bg_cover" style="background-image: url(<?php echo $focus_event_media_url[0]; ?>);">
+
+		<div class="moduleInner is-flex row">
+
+
+	    	<div class="focusEvent_infos m-3col m-1col-push">
+	    		<a href="<?php echo get_permalink($focus_event_id); ?>">
+	    			<span>Le prochain rendez-vous</span>
+	    			<h3><?php echo get_the_title($focus_event_id); ?></h3>
+						<span>Dates</span>
+	    		</a>
+	    	</div>
 
 
 
 		<?php if( have_rows('focus_elements', 'option') ): ?>
-			<div class="m-4col">
-					<div class="row">
+
 
 		   <?php while ( have_rows('focus_elements', 'option') ) : the_row();
 
 		        if( get_row_layout() == 'focusElements_page' ):
 		        	$focusElements_page = get_sub_field('focusElements_page'); ?>
 
-							<div class="m-1col">
+							<div class="focusElement_item m-1col m-1col-push">
+								<div class="focusElement_media bg_cover" style="background-image: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $focusElements_page->ID ), 'large' )[0]; ?>);">
+								</div>
+								
 					    	<h3><a href="<?php echo get_permalink( $focusElements_page->ID ); ?>"><?php echo $focusElements_page->post_title; ?></a></h3>
+
+					    	<p><?php echo 'chapo chapo chapo'; ?></p>
 					    </div>
 
 		        <?php elseif( get_row_layout() == 'focusElements_article' ): 
 		        	$focusElements_article = get_sub_field('focusElements_article'); ?>
 
-							<div class="m-1col">
+							<div class="focusElement_item m-1col m-1col-push">
+								<img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $focusElements_article->ID ), 'large' )[0]; ?>" alt="">
 					    	<h3><a href="<?php echo get_permalink( $focusElements_article->ID ); ?>"><?php echo $focusElements_article->post_title; ?></a></h3>
+					    	<p><?php echo 'chapo chapo chapo'; ?></p>
 					    </div>
 
 
 		        <?php elseif( get_row_layout() == 'focusElements_libre' ): 
 		        	$focusElements_libre = get_sub_field('focusElements_libre'); ?>
 							
-							<div class="m-1col">
+							<div class="focusElement_item m-1col m-1col-push">
 		        		<?php echo $focusElements_libre; ?>
 							</div>
 
@@ -53,8 +62,7 @@
 		        <?php endif;
 
 		    endwhile; ?>
-		    	</div>
-				</div>
+
 
 		<?php else :
 
@@ -64,5 +72,7 @@
 
 	</div>
 </section>
+
+		<?php endif; ?>
 
 
