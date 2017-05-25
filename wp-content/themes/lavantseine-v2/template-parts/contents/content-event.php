64c6834cc4f41094db36548399458af91203d33a
thomas <hello@thomasflorentin.net>
2017-05-25 12:10:45 +0200

    First templating & Polish for Event

--- wp-content/themes/lavantseine-v2/template-parts/contents/content-event.php
+++ wp-content/themes/lavantseine-v2/template-parts/contents/content-event.php
@@ -2,142 +2,71 @@
 /**
  * @package lavantseine
  */
+
+setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
+
+$post_meta_data = get_post_custom($post->ID);
+
+$tags = wp_get_post_terms($post->ID, array('discipline', 'rdv'), array("fields" => "all"));
+
+$event_dates = get_field( 'eventDetail_dates' );
+$event_duration = get_field( 'eventDetail_duration' );
+$event_text2 = get_field( 'eventDetail_text2' );
+$event_first_date = htmlspecialchars( get_field( 'eventDetail_first_date' ) );
+$event_last_date = htmlspecialchars( get_field( 'eventDetail_last_date' ) );
+$event_other_dates = unserialize( get_field('eventDetail_otherdates')[0]);
+$event_landscape_media = get_field( 'eventDetail_landscapeMedia' );
+
+$eventDetail_mediaMarkup = get_field( 'eventDetail_mediaMarkup' );
+$eventDetail_showPic = get_field( 'eventDetail_showPic' );
+
+$attached = get_post_meta(get_the_ID(), 'wp_custom_attachment', true);
+
+$event_distribution = get_field( 'eventDetail_distribution' );
+$event_mentions = get_field( 'eventDetail_mentions' );
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-		<div class=" wrap entry-media">
-			<?php
-				$eventDetail_mediaMarkup = get_field( 'eventDetail_mediaMarkup' );
-				$eventDetail_showPic = get_field( 'eventDetail_showPic' );
-				
-				get_template_part( 'part', 'postslide' );
-
-				if ( !$eventDetail_showPic ) {
-			  	the_post_thumbnail('top-thumbnail');
-				}
-				if ( $eventDetail_mediaMarkup ) {
-					echo $eventDetail_mediaMarkup;
-				}
-			?>
-		</div>
 
 
-		<header class=" wrap entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header><!-- .entry-header -->
-
-
-		<div class="wrap entry-content">
+	<header class="event-header bg_cover" style="background-image: url(<?php the_post_thumbnail_url('top-thumbnail'); ?>);">
+		<div class="eventHeader-inner row wrap is-flex">
 			
-			<div title="Add to Calendar" class="addeventatc">
-		    Ajouter à mon calendrier
-		    <span class="start">06/05/2017 09:00 AM</span>
-		    <span class="end">06/05/2017 11:00 AM</span>
-		    <span class="timezone">Europe/Paris</span>
-		    <span class="title"><?php the_title(); ?></span>
-		    <span class="description">Description of the event</span>
-		    <span class="location">l'Avant-Seine, théâtre de Colombes</span>
-		    <span class="organizer">l'Avant-Seine, théâtre de Colombes</span>
-		    <span class="organizer_email">Organizer e-mail</span>
-		    <span class="facebook_event">https://www.facebook.com/events/703782616363133</span>
-		    <span class="all_day_event">true</span>
-		    <span class="date_format">MM/DD/YYYY</span>
-		    <span class="alarm_reminder">15</span>
-		    <span class="recurring">FREQ=DAILY;COUNT=10</span>
-		    <span class="calname">Custom event filename</span>
-		    <span class="uid">event123</span>
-		    <span class="status">confirmed</span>
-		    <span class="client">asIudnvhizljTCuevmzc28585</span>
-		    <span class="method">REQUEST</span>
-			</div> 
+			<div class="eventHeader-title m-5col">
+				<h1 class="h1 entry-title"><?php the_title(); ?></h1>
+			</div>
+
+			<div class="eventHeader-dates m-3col">
+				<?php 
+
+					// if ( $event_dates ) : echo "<span class='date-main'>". $event_dates ."</span>" ; endif; 
+					if ( $event_first_date ) : 
+						echo '<ul class="event-repeatable-dates">';
+						    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_first_date ) .'.</li>'; 
+
+						if ( $event_other_dates ) : 
+							foreach ($event_other_dates as $date) { 
+								$date = strtotime($date);
+							    if ( $date != '' ) : 
+							    	echo '<li>'. strftime('%A %e %b %G - %kh%M', $date ) .'.</li>'; 
+							    endif;
+							} 
+						endif; 
+
+						if ( $event_last_date && $event_last_date != $event_first_date ) : 
+						    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_last_date ) .'.</li>'; 							    
+						endif;
+
+						echo '</ul>';
+					endif; 
+				?>
+
+				<?php if ( $event_duration ) : echo "<span class='eventHeader-duration'> Durée : ". $event_duration ."</p>"; endif; ?>
 
 
-				<div class="">
-					<?php
-						setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
-
-						$post_meta_data = get_post_custom($post->ID);
-		
-						$tags = wp_get_post_terms($post->ID, array('discipline', 'rdv'), array("fields" => "all"));
-
-						$event_dates = get_field( 'eventDetail_dates' );
-						$event_duration = get_field( 'eventDetail_duration' );
-						$event_text2 = get_field( 'eventDetail_text2' );
-						$event_first_date = htmlspecialchars( get_field( 'eventDetail_first_date' ) );
-						$event_last_date = htmlspecialchars( get_field( 'eventDetail_last_date' ) );
-						$event_other_dates = unserialize( get_field('eventDetail_otherdates')[0]);
-						$event_landscape_media = get_field( 'eventDetail_landscapeMedia' );
-
-
-							// IF NEW DATE METHOD IS SET 
-
-							if( have_rows('eventDetail_newdates') ):
-
-						    while ( have_rows('eventDetail_newdates') ) : the_row();
-
-					        the_sub_field('date');
-					        echo ' - ';
-									the_sub_field('horaire');
-									echo '<br>';
-
-							   endwhile;
-
-							else :
-
-							// IF OLD DATE METHOD 
-								// if ( $event_dates ) : echo "<span class='date-main'>". $event_dates ."</span>" ; endif; 
-								if ( $event_first_date ) : 
-									echo '<ul class="event-repeatable-dates">';
-									    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_first_date ) .'.</li>'; 
-
-									if ( $event_other_dates ) : 
-										foreach ($event_other_dates as $date) { 
-											$date = strtotime($date);
-										    if ( $date != '' ) : 
-										    	echo '<li>'. strftime('%A %e %b %G - %kh%M', $date ) .'.</li>'; 
-										    endif;
-										} 
-									endif; 
-
-									if ( $event_last_date && $event_last_date != $event_first_date ) : 
-									    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_last_date ) .'.</li>'; 							    
-									endif;
-
-									echo '</ul>';
-								endif; 
-
-
-							endif; ?>
-
-						<?php 
-						$count = count($tags);
-						if ( $count > 0 ){
-						    echo "<ul class='tags-list'>";
-						    foreach ( $tags as $term ) {
-				    			$term_link = get_term_link( $term, '' );
-						    	echo "<a href='". $term_link ."'><li class='saisoned-on-color'>#" . $term->name . "</li></a>";
-						    	echo $term->description;
-						    }
-						    echo "</ul>";
-						} 
-						?>
-
-						<?php if ( $event_duration ) : echo "<p> Durée : ". $event_duration ."</p>"; endif; ?>
-
-						<?php $attached = get_post_meta(get_the_ID(), 'wp_custom_attachment', true); ?> 
-						<?php if ($attached) : ?>
-						<p class="attached-file"><a href="<?php echo $attached['url']; ?>">  
-						    Téléchargez le dossier de presse
-						</a></p>
-						<?php endif; ?>
-
-						<?php if ( $event_text2 ) : echo "<p class=''>". $event_text2 ."</p>"; endif; ?>
-
-				</div><!-- .event-tags -->
-
-				<div class="">
-					<?php
+				<?php
 					$publics =  get_the_terms( $post->ID, 'public' );
 					if($publics) {
 					  foreach ($publics as $public) {
@@ -153,38 +82,152 @@
    					  echo '</div>';
 					  }
 					}
+				?>
+
+
+				<div title="Add to Calendar" class="addeventatc">
+			    Ajouter à mon calendrier
+			    <span class="start">06/05/2017 09:00 AM</span>
+			    <span class="end">06/05/2017 11:00 AM</span>
+			    <span class="timezone">Europe/Paris</span>
+			    <span class="title"><?php the_title(); ?></span>
+			    <span class="description">Description of the event</span>
+			    <span class="location">l'Avant-Seine, théâtre de Colombes</span>
+			    <span class="organizer">l'Avant-Seine, théâtre de Colombes</span>
+			    <span class="organizer_email">Organizer e-mail</span>
+			    <span class="facebook_event">https://www.facebook.com/events/703782616363133</span>
+			    <span class="all_day_event">true</span>
+			    <span class="date_format">MM/DD/YYYY</span>
+			    <span class="alarm_reminder">15</span>
+			    <span class="recurring">FREQ=DAILY;COUNT=10</span>
+			    <span class="calname">Custom event filename</span>
+			    <span class="uid">event123</span>
+			    <span class="status">confirmed</span>
+			    <span class="client">asIudnvhizljTCuevmzc28585</span>
+			    <span class="method">REQUEST</span>
+				</div> 
+
+			</div>
+
+		</div>
+	</header><!-- .event-header -->
+
+
+
+	<div class="event-meta event-layer wrap">
+		<?php 
+			$count = count($tags);
+			if ( $count > 0 ){
+			    echo "<ul class='no-bullets'>";
+			    foreach ( $tags as $term ) {
+		  			$term_link = get_term_link( $term, '' );
+			    	echo "<li class='meta-term'>" . $term->name . "</li>";
+			    	echo $term->description;
+			    }
+			    echo "</ul>";
+			} 
+		?>
+	</div><!-- .event-meta -->
+
+
+	
+
+	<div class="event-content event-layer wrap row">
+		
+		<div class="m-5col">
+			<?php 
+				the_content(); 
+				
+				if ( $eventDetail_mediaMarkup ) {
+					echo $eventDetail_mediaMarkup;
+				}
+
+				get_template_part( 'part', 'postslide' );
+			?>
+			
+			<a class="btn--big" href="<?php bloginfo('url'); ?>/les-infos-pratiques/tarifs-et-reservations/" class="button saisoned-on-bg">Réservation</a>
+
+		</div>
+
+
+		<div class="m-3col">
+			<h3 class="h2">autour du <br>spectacle</h3>
+
+		</div>
+	</div><!-- .event-content -->
+
+
+
+	<div class="event-details event-layer clearfix">
+		<div class="wrap">
+			
+			<div class="row clearfix">
+				<div class="m-2col">
+					<h4 class="h5">Tarifs</h4>
+
+					<?php				
+						$term_list = wp_get_post_terms($post->ID, 'tarif', array("fields" => "all"));
+						$count = count($term_list);
+						if ( $count > 0 ){
+						    echo "<ul class='no-bullets'>";
+						    foreach ( $term_list as $term ) {
+						    	echo "<li class=''>#" . $term->name . "</li>";
+						    	echo $term->description;
+						    }
+						    echo "</ul>";
+						}
 					?>
 
 				</div>
 
+				<div class="m-3col">
+					<h4 class="h5">Dates</h4>
 
-			<div class=" ">
-				<?php the_content(); ?>
+
+				</div>
 			</div>
-		
-		</div><!-- .entry-content -->
+			
+
+
+			<div class="row clearfix">
+				<h4 class="h5">Distribution et mentions complètes</h4>
+	
+				<div class="m-2col m-first">
+						<?php if ( $event_text2 ) : echo "<p class=''>". $event_text2 ."</p>"; endif; ?>
+
+						<?php 
+							if ( $event_distribution || $event_mentions ) : 
+								echo $event_distribution;
+							endif; ?>
+
+				</div>
+
+				<div class="m-3col">
+
+						<?php 
+							if (  $event_mentions ) : 
+								echo $event_mentions;
+							endif; ?>
+
+						<?php if ($attached) : ?>
+						<p class="attached-file"><a href="<?php echo $attached['url']; ?>">  
+						    Téléchargez le dossier de presse
+						</a></p>
+						<?php endif; ?>
+				</div>
+			</div>
 
 
 
 
-		<div class="wrap entry-meta">
+	
+		</div>
+	</div><!-- .event-details -->
 
-			<div class="">
-				<?php
 
-				// TODO => template tags
-				
-					$term_list = wp_get_post_terms($post->ID, 'tarif', array("fields" => "all"));
-					$count = count($term_list);
-					if ( $count > 0 ){
-					    echo "<ul class='tags-list'>";
-					    foreach ( $term_list as $term ) {
-					    	echo "<li class='saisoned-on-color'>#" . $term->name . "</li>";
-					    	echo $term->description;
-					    }
-					    echo "</ul>";
-					}
-				?>
+
+
+
 			</div><!-- .event-price -->
 
 
@@ -195,7 +238,7 @@
 					$event_dealer_name = get_field( 'eventDetail_dealer-name' );
 				?>		
 
-				<button href="<?php bloginfo('url'); ?>/les-infos-pratiques/tarifs-et-reservations/" class="button saisoned-on-bg">Réservez au Théâtre</button>
+				
 				<?php 
 					if ( $event_dealer_name ) : ?>
 						<button href="<?php echo $event_dealer_link; ?>" target="_blank" class="button saisoned-on-bg">Réservez sur <?php echo $event_dealer_name; ?></button>
@@ -212,20 +255,7 @@
 
 
 
-		<?php $event_distribution = get_field( 'eventDetail_distribution' ); ?>
-		<?php $event_mentions = get_field( 'eventDetail_mentions' ); ?>
-		<?php if ( $event_distribution || $event_mentions ) : ?>
-		<footer class=" wrap entry-mentions">
-			<p id="display-entry-mentions"><a href="#">+ distributions et mentions complètes</a></p>
-			
-			<div id="inner-entry-mentions">
-				<?php
-						echo "<p>". $event_distribution ."</p>";
-						echo "<p>". $event_mentions ."</p>";
-				?>
-			</div>
-		</footer><!-- .entry-mentions -->
-		<?php endif; ?>
+
 
 
 
