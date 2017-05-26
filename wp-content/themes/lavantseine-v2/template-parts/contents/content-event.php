82cddf3510f7f961e06bc7ade7dee880bea895c8
thomas <hello@thomasflorentin.net>
2017-05-26 12:43:59 +0200

    Test for layer

--- wp-content/themes/lavantseine-v2/template-parts/contents/content-event.php
+++ wp-content/themes/lavantseine-v2/template-parts/contents/content-event.php
@@ -31,7 +31,6 @@ $event_mentions = get_field( 'eventDetail_mentions' );
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 
-
 	<header class="event-header bg_cover" style="background-image: url(<?php the_post_thumbnail_url('top-thumbnail'); ?>);">
 		<div class="eventHeader-inner row wrap is-flex">
 			
@@ -94,130 +93,150 @@ $event_mentions = get_field( 'eventDetail_mentions' );
 
 
 
-	<div class="event-layer wrap row">
-		
-		<div class="m-5col event-content">
-			<?php 
-				the_content(); 
-				if ( $eventDetail_mediaMarkup ) {
-					echo $eventDetail_mediaMarkup;
-				}
-				get_template_part( 'part', 'postslide' );
-			?>
-			
+	<div class="event-layer ">
 
-				<?php
-					$publics =  get_the_terms( $post->ID, 'public' );
-					if($publics) {
-					  foreach ($publics as $public) {
-					  	echo '<div class="event-public-item">';
-						    $tax_term_id = $public->term_taxonomy_id;
-						    $images = get_option('taxonomy_image_plugin');
-						    
-						    echo '<span class="public-label">A partir de</span>';
-						    echo '<div class="public-img">';
-						    	echo wp_get_attachment_image( $images[$tax_term_id], '' );
-						    	echo '<p class="public-name">'. $public->name .'</p>';
-						    echo '</div>';
-   					  echo '</div>';
-					  }
+		<div class="layer-bg">
+			<div class="row">
+				<div class="m-5col"></div>
+				<div class="m-3col m-last"></div>
+			</div>
+		</div>
+
+		<div class="wrap row">
+				
+			<div class="m-5col ">
+				<div class="event-content"><?php 
+					the_content(); ?></div>
+				<div>
+					<?php
+					if ( $eventDetail_mediaMarkup ) {
+						echo $eventDetail_mediaMarkup;
 					}
+					get_template_part( 'part', 'postslide' );
 				?>
+				</div>
 
-			<a class="btn--big" href="<?php bloginfo('url'); ?>/les-infos-pratiques/tarifs-et-reservations/" class="button saisoned-on-bg">Réservation</a>
-		</div>
-
-
-		<div class="m-3col event-aside">
-			<h3 class="h2">autour du <br>spectacle</h3>
-
-				<?php set_query_var('taxo', 'relational_tag'); ?>
-				<?php get_template_part( 'template-parts/modules/module', 'relatedposts' ); ?>
-
-		</div>
-	</div><!-- .event-content -->
-
-
-
-	<div class="event-details event-layer clearfix">
-		<div class="wrap">
-			
-			<div class="row clearfix">
-				<div class="m-2col">
-					<h4 class="h5">Tarifs</h4>
-
-					<?php				
-						$term_list = wp_get_post_terms($post->ID, 'tarif', array("fields" => "all"));
-						$count = count($term_list);
-						if ( $count > 0 ){
-						    echo "<ul class='no-bullets'>";
-						    foreach ( $term_list as $term ) {
-						    	echo "<li class=''>#" . $term->name . "</li>";
-						    	echo $term->description;
-						    }
-						    echo "</ul>";
+					<?php
+						$publics =  get_the_terms( $post->ID, 'public' );
+						if($publics) {
+						  foreach ($publics as $public) {
+						  	echo '<div class="event-public-item">';
+							    $tax_term_id = $public->term_taxonomy_id;
+							    $images = get_option('taxonomy_image_plugin');
+							    
+							    echo '<span class="public-label">A partir de</span>';
+							    echo '<div class="public-img">';
+							    	echo wp_get_attachment_image( $images[$tax_term_id], '' );
+							    	echo '<p class="public-name">'. $public->name .'</p>';
+							    echo '</div>';
+	   					  echo '</div>';
+						  }
 						}
 					?>
-				</div>
 
-				<div class="m-3col">
-					<h4 class="h5">Dates</h4>
+				<a class="btn--big" href="<?php bloginfo('url'); ?>/les-infos-pratiques/tarifs-et-reservations/" class="button saisoned-on-bg">Réservation</a>
 
-					<?php 
-						if ( $event_first_date ) : 
-							echo '<ul class="no-bullets">';
-							    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_first_date ) .'.</li>'; 
 
-							if ( $event_other_dates ) : 
-								foreach ($event_other_dates as $date) { 
-									$date = strtotime($date);
-								    if ( $date != '' ) : 
-								    	echo '<li>'. strftime('%A %e %b %G - %kh%M', $date ) .'.</li>'; 
-								    endif;
-								} 
-							endif; 
+				<div class="event-details clearfix">
+					<div class="">
+						
+						<div class="row clearfix">
+							<div class="m-2col">
+								<h4 class="h5">Tarifs</h4>
 
-							if ( $event_last_date && $event_last_date != $event_first_date ) : 
-							    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_last_date ) .'.</li>'; 							    
-							endif;
+								<?php				
+									$term_list = wp_get_post_terms($post->ID, 'tarif', array("fields" => "all"));
+									$count = count($term_list);
+									if ( $count > 0 ){
+									    echo "<ul class='no-bullets'>";
+									    foreach ( $term_list as $term ) {
+									    	echo "<li class=''>#" . $term->name . "</li>";
+									    	echo $term->description;
+									    }
+									    echo "</ul>";
+									}
+								?>
+							</div>
 
-							echo '</ul>';
-						endif; 
-					?>
+							<div class="m-3col">
+								<h4 class="h5">Dates</h4>
 
-				</div>
+								<?php 
+									if ( $event_first_date ) : 
+										echo '<ul class="no-bullets">';
+										    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_first_date ) .'.</li>'; 
+
+										if ( $event_other_dates ) : 
+											foreach ($event_other_dates as $date) { 
+												$date = strtotime($date);
+											    if ( $date != '' ) : 
+											    	echo '<li>'. strftime('%A %e %b %G - %kh%M', $date ) .'.</li>'; 
+											    endif;
+											} 
+										endif; 
+
+										if ( $event_last_date && $event_last_date != $event_first_date ) : 
+										    echo '<li>'. strftime('%A %e %b %G - %kh%M', $event_last_date ) .'.</li>'; 							    
+										endif;
+
+										echo '</ul>';
+									endif; 
+								?>
+
+							</div>
+						</div>
+						
+
+						<div class="row clearfix">
+							<h4 class="h5">Distribution et mentions complètes</h4>
+				
+							<div class="m-2col m-first">
+									<?php if ( $event_text2 ) : echo "<p class=''>". $event_text2 ."</p>"; endif; ?>
+
+									<?php 
+										if ( $event_distribution || $event_mentions ) : 
+											echo $event_distribution;
+										endif; ?>
+							</div>
+
+							<div class="m-3col">
+
+									<?php 
+										if (  $event_mentions ) : 
+											echo $event_mentions;
+										endif; ?>
+
+									<?php if ($attached) : ?>
+									<p class="attached-file"><a href="<?php echo $attached['url']; ?>">  
+									    Téléchargez le dossier de presse
+									</a></p>
+									<?php endif; ?>
+							</div>
+						</div>
+
+					</div>
+				</div><!-- .event-details -->
+
+
+
+
 			</div>
-			
 
-			<div class="row clearfix">
-				<h4 class="h5">Distribution et mentions complètes</h4>
-	
-				<div class="m-2col m-first">
-						<?php if ( $event_text2 ) : echo "<p class=''>". $event_text2 ."</p>"; endif; ?>
 
-						<?php 
-							if ( $event_distribution || $event_mentions ) : 
-								echo $event_distribution;
-							endif; ?>
-				</div>
+			<div class="m-3col event-aside">
+				<h3 class="h2">autour du <br>spectacle</h3>
 
-				<div class="m-3col">
+					<?php set_query_var('taxo', 'relational_tag'); ?>
+					<?php get_template_part( 'template-parts/modules/module', 'relatedposts' ); ?>
 
-						<?php 
-							if (  $event_mentions ) : 
-								echo $event_mentions;
-							endif; ?>
-
-						<?php if ($attached) : ?>
-						<p class="attached-file"><a href="<?php echo $attached['url']; ?>">  
-						    Téléchargez le dossier de presse
-						</a></p>
-						<?php endif; ?>
-				</div>
 			</div>
-
 		</div>
-	</div><!-- .event-details -->
+
+	</div><!-- .event-layer -->
+
+
+
+
 
 
 </article><!-- #post-## -->
