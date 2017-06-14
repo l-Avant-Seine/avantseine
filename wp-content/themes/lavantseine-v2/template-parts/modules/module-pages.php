babf453fbe4498e3ba9acbba731db3fe604ecc08
thomas <hello@thomasflorentin.net>
2017-06-14 10:34:31 +0200

    Polish hompage

--- wp-content/themes/lavantseine-v2/template-parts/modules/module-pages.php
+++ wp-content/themes/lavantseine-v2/template-parts/modules/module-pages.php
@@ -16,7 +16,7 @@
 				
 				<h3 class="h5 modulePages-title">&#x02666;<br><?php the_title(); ?></h3>
 				<p class="modulePages-excerpt"><?php the_excerpt(); ?></p>
-				<a href="<?php the_permalink(); ?>" class="btn--little"><span class="icon-arrow"></span>En savoir plus</a>
+				<a href="<?php the_permalink(); ?>" class="btn--little"><span class="icon-arrow-right"></span>En savoir plus</a>
 
 			</div>
 		<?php endforeach; ?>
--- wp-content/themes/lavantseine-v2/template-parts/modules/module-services.php
+++ wp-content/themes/lavantseine-v2/template-parts/modules/module-services.php
@@ -4,14 +4,14 @@
 <section class="module-services offset-right">
 	<div class="moduleInner">
 		
-		<h4 class="h4 module-title">à votre <br>service <br><span class="title-diamond">&#x02666;</span></h4>
+		<h4 class="h4 module-title">à votre <br>service <span class="title-diamond">&#x02666;</span></h4>
 
 
 		<?php foreach ( $services_pages as $post ) : setup_postdata( $post ); ?>
 			<div class="moduleService-item">
 				<h5 class="h5 moduleService-title"><?php the_title(); ?></h5>
-				<p class="moduleService-excerpt"><?php the_excerpt(); ?></p>
-				<a href="<?php the_permalink(); ?>" class="btn--little">En savoir plus</a>
+				<div class="moduleService-excerpt"><?php the_excerpt(); ?></div>
+				<a href="<?php the_permalink(); ?>" class="btn--little"><span class="icon-arrow-right"></span>En savoir plus</a>
 			</div>
 		<?php endforeach; ?>
 
