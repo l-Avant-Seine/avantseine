fd281ec9b22d3ea4d5fe17da3a22b56254ab83aa
thomas <hello@thomasflorentin.net>
2017-06-22 16:44:06 +0200

    Add template tags for schema.org

--- wp-content/themes/lavantseine-v2/inc/template-tags.php
+++ wp-content/themes/lavantseine-v2/inc/template-tags.php
@@ -9,6 +9,41 @@
 
 
 
+
+function html_tag_schema()
+{
+    $schema = 'http://schema.org/';
+
+    // Is of book post type
+    if(is_singular('event'))
+    {
+        $type = 'Event';
+    }
+    // Is single post
+    elseif(is_single())
+    {
+        $type = "Article";
+    }
+    // Is author page
+    elseif( is_author() )
+    {
+        $type = 'ProfilePage';
+    }
+    // Is search results page
+    elseif( is_search() )
+    {
+        $type = 'SearchResultsPage';
+    }
+    else
+    {
+        $type = 'WebPage';
+    }
+
+    echo 'itemscope="itemscope" itemtype="' . $schema . $type . '"';
+}
+
+
+
 /*
  * Social Buttons Sharing
  */
