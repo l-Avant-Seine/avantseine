ca8a716f6526b187b5843cf891e4bd7e49f44b5b
thomas <hello@thomasflorentin.net>
2017-09-13 10:31:46 +0200

    Update yaost plugin

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitb475fed6016da8fa03e743e036b80517::getLoader();
+return ComposerAutoloaderInit072f4fc627df93445aa7d4702d387818::getLoader();
