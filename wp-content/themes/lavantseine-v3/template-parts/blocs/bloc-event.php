99aa08af908c0e00ee430b938e03bc52b4d64876
thomas <hello@thomasflorentin.net>
2019-04-25 14:33:10 +0200

    fix event date for booking

--- wp-content/themes/lavantseine-v3/template-parts/blocs/bloc-event.php
+++ wp-content/themes/lavantseine-v3/template-parts/blocs/bloc-event.php
@@ -8,8 +8,9 @@
 	$event_text2 = get_field( 'eventDetail_text2' );
 	$event_first_date = htmlspecialchars( get_field( 'eventDetail_first_date' ) );
 	$event_last_date = htmlspecialchars( get_field( 'eventDetail_last_date' ) );
+	 $event_last_date = htmlspecialchars( get_field( 'eventDetail_last_date' ) );
 	$event_other_dates = get_field('eventDetail_otherdates');
-$today = time();
+	$today = time();
 	$event_dealer_link = get_field( 'eventDetail_dealer-link' );
 
 	$event_landscape_media = get_post_meta( $post->ID, 'eventMedia_landscape', true );
@@ -35,7 +36,7 @@ $today = time();
 					<div class="blocEvent-actions">
 						<a href="<?php the_permalink(); ?>" class="btn--big empty">en savoir plus</a>
 
-						<?php if( intval($event_first_date) > $today ) : ?>
+						<?php if( intval($event_last_date) > $today ) : ?>
 							<a href="<?php echo $event_dealer_link; ?>" target="_blank" class="btn--big">réserver mes places</a>
 						<?php endif; ?>
 
