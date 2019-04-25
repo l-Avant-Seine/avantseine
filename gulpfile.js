b6c488b8ffd0f115e9409b949329ff600bf497e9
thomas <hello@thomasflorentin.net>
2019-04-25 14:57:22 +0200

    cleanup da mess
    remove styles
    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,65 @@
-// Include gulp
-var gulp = require('gulp');
-
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-ruby-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var cssnano = require('gulp-cssnano');
-var notify = require('gulp-notify');
-
-// Set up project
-var app_folder = 'wp-content/themes/lavantseine-v3/';
-var public_folder = 'wp-content/themes/lavantseine-v3/';
-var theme_folder = 'wp-content/themes/lavantseine-v3/';
-var assets_folder = 'wp-content/themes/lavantseine-v3/assets/';
-
-var jsfolder = assets_folder + 'js/';
-var mainjs = assets_folder + 'js/scripts.js';
-var vendorsjs = assets_folder + 'js/lib/*.js';
-var alljs = [vendorsjs, mainjs];
-
-var scssfiles = assets_folder + 'css/**/*.scss';
-var sassfolder = assets_folder + 'css/';
-var sassMain = sassfolder + 'style.scss';
-
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src(mainjs)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-// Compile Our Sass
-gulp.task('styles', function() {
-    return sass(sassMain, { style: 'nested', sourcemap: true })
-    .on('error', sass.logError)
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(theme_folder))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(cssnano())
-    .pipe(gulp.dest(theme_folder))
-    .pipe(notify({ message: 'Styles task complete' }));
-});
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(alljs)
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest(jsfolder))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsfolder))
-        .pipe(notify({ message: 'Scripts task complete' }));
-});
+// gulp.task('scripts', function() {
+//     return gulp.src(alljs)
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest(jsfolder))
+//         .pipe(rename('all.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest(jsfolder))
+//         .pipe(notify({ message: 'Scripts task complete' }));
+// });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch(mainjs, ['lint', 'scripts']);
-    gulp.watch(scssfiles, ['styles']);
-});
+// gulp.task('watch', function() {
+//     gulp.watch(mainjs, ['lint', 'scripts']);
+//     gulp.watch(scssfiles, ['styles']);
+// });
 
 // Default Task
-gulp.task('default', ['lint', 'scripts', 'watch']);
+// gulp.task('default', ['lint', 'scripts', 'watch']);
+
+
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+    
+var paths = {
+    styles: {
+        src: "wp-content/themes/lavantseine-v3/assets/css/**/*.sass",
+        dest: "wp-content/themes/lavantseine-v3/assets/"
+    }
+};
+
+
+    
+function style() {
+    
+    return (
+        gulp
+            .src(paths.styles.src)
+            // Initialize sourcemaps before compilation starts
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+    );
+    
+}
+
+    
+function watch() {
+    style();
+    
+    gulp.watch(paths.styles.src, style);
+}
+
+    
+exports.watch = watch
 
