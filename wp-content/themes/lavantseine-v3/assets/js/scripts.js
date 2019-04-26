2ec7f2b1af50fc21997af162bbbac5616d8c6080
thomas <hello@thomasflorentin.net>
2019-04-26 13:14:57 +0200

    Navigations

--- wp-content/themes/lavantseine-v3/assets/js/scripts.js
+++ wp-content/themes/lavantseine-v3/assets/js/scripts.js
@@ -7,5 +7,14 @@ jQuery(function($) {
 	console.log('hello');
 
 
+		$(window).scroll(function(){
+			if ( jQuery(this).scrollTop() !== 0 ) {
+	      $('body').addClass('scrolling');
+	    } else {
+	    	$('body').removeClass('scrolling');
+	    }
+    });
+    
+
 });
 
--- wp-content/themes/lavantseine-v3/header.php
+++ wp-content/themes/lavantseine-v3/header.php
@@ -35,6 +35,15 @@ fbq('track', 'PageView');
  <img height="1" width="1" 
 src="https://www.facebook.com/tr?id=1951786325076755&ev=PageView&noscript=1"/>
 </noscript>
+
+				<?php
+				if ( wp_is_mobile() ) {
+					$header_file = get_bloginfo( 'template_url' ) . '/assets/img/avtseine-logo-2019.png';
+				} else {
+					$header_file = get_bloginfo( 'template_url' ) . '/assets/img/avtseine-logo-2019.png';
+				}
+				?>
+				
 <!-- End Facebook Pixel Code -->
 
 </head>
@@ -42,19 +51,19 @@ src="https://www.facebook.com/tr?id=1951786325076755&ev=PageView&noscript=1"/>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header cf" role="banner">
 
-		<div class="site-menus">
+		<nav class="site-branding">
+			<a href="<?php echo esc_url( home_url( '/' ) ) ?>" rel="home">
+				<img class="site-logo" src="<?php echo $header_file ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" title="" width="100">
+			</a>
+		</nav>
 
-			<nav id="site-navigation" class="siteMenus-primary" role="navigation">
+		<div class="site-menus is-flex">
 
-				<?php
-				if ( wp_is_mobile() ) {
-					$header_file = get_bloginfo( 'template_url' ) . '/assets/img/logo_avtseine_horizontal.png';
-				} else {
-					$header_file = get_bloginfo( 'template_url' ) . '/assets/img/logo_avtseine_vertical.png';
-				}
-				?>
+			<nav id="site-navigation" class="cf siteMenus-primary" role="navigation">
+
+
 
 				<?php 
 					wp_nav_menu( array( 
@@ -62,54 +71,16 @@ src="https://www.facebook.com/tr?id=1951786325076755&ev=PageView&noscript=1"/>
 						'menu_id' => 'primary-menu',
 						'walker' => new Microdot_Walker_Nav_Menu(), 
 						'container' => false, 
-						'items_wrap' => '<ul class="no-bullets">
-																<li class="site-branding menu-item">
-																	<a href="'. esc_url( home_url( '/' ) ) .'" rel="home">
-																		<img class="site-logo" src="' . $header_file . '" alt="'. get_bloginfo( 'name' ) .'" title="">
-																	</a>
-																</li>
+						'items_wrap' => '<ul class="siteMenuPrimary-inner menu-item no-bullets">
+																	%3$s
 
 																<li class="js-menuTrigger menu-item">
-																	<a href="#"> <span class="icon-menu"></span> Menu</a>
+																	<a href="#"> <span class="icon-menu"></span> et aussi</a>
 																</li>
-
-																<ul class="siteMenuPrimary-inner menu-item no-bullets">
-																	%3$s
-																</ul>
-															
-																<ul class="siteMenus-secondary no-bullets" role="navigation">
-
-																			<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
-																				<a href="https://www.facebook.com/lAvantSeine/" target="_blank"><span class="icon-facebook">
-																					</span></a>
-																			</li>
-
-																			<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
-																				<a href="https://twitter.com/AvantSeine" target="_blank"><span class="icon-twitter">
-																					</span></a>
-																			</li>
-
-																			<li id="menu-item-9455" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455"><a href="http://lavant-seine.com/lieu/equipe//">Contacts</a></li>
-
-																			<li id="menu-item-9454" class="booking-link menu-item menu-item-type-custom menu-item-object-custom menu-item-9454"><a target="_blank" href="http://billetterie.lavant-seine.com" target="_blank"><span class="icon-pop-out"></span> Réserver</a></li>
-
-																			<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
-																				<a href="#" id="js-searchTrigger"><span class="icon-search">
-																					</span></a>
-																			</li>
-
-																	<div class="siteMenus-searchform">
-																		<form id="searchform" class="searchbar" action="/" method="get">
-																	    <input type="text" name="s" id="search" placeholder="votre recherche" value="" />
-																	    <input type="submit" alt="Search" class="" value="ok" />
-																	</form>
-																	</div>
-																	
-																</ul>
 															</ul>'
 					) ); ?>
 
-				<div id="ham-menu" class="ham-menu">
+				<div id="ham-menu" class="siteMenus-large">
 					<div class="wrap row">
 						<?php 
 							wp_nav_menu( array( 
@@ -131,7 +102,6 @@ src="https://www.facebook.com/tr?id=1951786325076755&ev=PageView&noscript=1"/>
 																		<a href="https://www.facebook.com/lAvantSeine/" target="_blank"><span class="icon-facebook"></span></a>
 																		<a href="https://twitter.com/AvantSeine" target="_blank"><span class="icon-twitter"></span></a>
 																	</li>
-
 																</ul>'
 								 ) 
 							); 
@@ -141,8 +111,43 @@ src="https://www.facebook.com/tr?id=1951786325076755&ev=PageView&noscript=1"/>
 
 			</nav><!-- #site-navigation -->
 
+			<nav class="siteMenus-actions">
+				<ul class="nobullets">
+					<li id="menu-item-9455" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455"><a href="http://lavant-seine.com/lieu/equipe/" class="btn-primary">nous contacter</a></li>
 
-		</div>
+					<li id="menu-item-9454" class="booking-link menu-item menu-item-type-custom menu-item-object-custom menu-item-9454"><a target="_blank" href="http://billetterie.lavant-seine.com" target="_blank" class="btn-primary">réserver</a></li>
+				</ul>
+			</nav>
+
+			<nav class="siteMenus-contacts">
+				<ul class=" no-bullets" role="navigation">
+
+					<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
+						<a href="https://www.facebook.com/lAvantSeine/" target="_blank"><span class="icon-facebook">
+							</span></a>
+					</li>
+
+					<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
+						<a href="https://twitter.com/AvantSeine" target="_blank"><span class="icon-twitter">
+							</span></a>
+					</li>
+
+					<li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9455">
+						<a href="#" id="js-searchTrigger"><span class="icon-search">
+							</span></a>
+					</li>
+
+					<div class="siteMenus-searchform">
+						<form id="searchform" class="searchbar" action="/" method="get">
+					    <input type="text" name="s" id="search" placeholder="votre recherche" value="" />
+					    <input type="submit" alt="Search" class="" value="ok" />
+					</form>
+					</div>
+																	
+				</ul>
+			</nav>
+
+		</div><!-- .site-menus -->
 
 	</header><!-- #masthead -->
 
