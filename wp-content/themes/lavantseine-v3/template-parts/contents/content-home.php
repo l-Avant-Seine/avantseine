7abe8d9f7e3409d1e781259f432fc1aa3fa837ec
thomas <hello@thomasflorentin.net>
2021-11-09 11:21:22 +0100

    Add fileds for ticker

--- wp-content/themes/lavantseine-v3/template-parts/contents/content-home.php
+++ wp-content/themes/lavantseine-v3/template-parts/contents/content-home.php
@@ -17,10 +17,22 @@
 
 							<?php for ($i=0; $i < 50; $i++) { 
 								if( $i % 2 === 0 ) { ?>
-									<span class="ticker__item">Bienvenue !</span>
+									<span class="ticker__item">
+										<?php if( get_field('thicker_text_a', 'option') !== '' ) { 
+											the_field('thicker_text_a', 'option');
+										} else {
+											printf('Bienvenue !');
+										} ?>
+									</span>
 								<?php }
 								else { ?>
-									<span class="ticker__item red">Bienvenue !</span>
+									<span class="ticker__item red">
+										<?php if( get_field('thicker_text_b', 'option') !== '' ) { 
+											the_field('thicker_text_b', 'option');
+										} else {
+											printf('Bienvenue !');
+										} ?>
+									</span>
 								<?php }
 							} ?>	
 						</div>
