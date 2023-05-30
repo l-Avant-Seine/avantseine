18f85efabd0a14ca73d8e615dbb95516b82d6fe9
thomas <hello@thomasflorentin.net>
2023-05-30 15:32:09 +0200

    update News Form code

--- wp-content/themes/lavantseine-v3/template-parts/blocs/bloc-newsletter.php
+++ wp-content/themes/lavantseine-v3/template-parts/blocs/bloc-newsletter.php
@@ -1,7 +1,11 @@
 <!-- AvantSeine Signup Form HTML Code -->
 <div class="bloc-newsletter">
-<iframe class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/wIn/hhl" width="100%"></iframe>
 
-<script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
+<iframe data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://1hj9.mj.am/wgt/1hj9/mqz/form?c=2440e869" width="100%" style="height: 0;"></iframe>
+
+<script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script> 
+
+
+
 </div>
 
