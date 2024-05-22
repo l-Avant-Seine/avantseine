4b25a8db352ec93194d2b10e5e5c0f994bcee4b4
Thomas <hello@thomasflorentin.net>
2024-05-22 15:52:17 +0200

    updates: Yoast SEO Version 22.7

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitad0d526fdf070763dc913573c8571e8a::getLoader();
+return ComposerAutoloaderInitdde61db2dc32a7876d16ff8608905779::getLoader();
