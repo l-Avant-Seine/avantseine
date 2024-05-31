67b3c526d53e8722ed6cd27eda281a2cc11d432b
Thomas <hello@thomasflorentin.net>
2024-05-31 18:26:54 +0200

    pugins: Updates

---
+++ wp-content/plugins/matomo/app/core/Access/Role.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Access;
+
+abstract class Role
+{
+    public abstract function getName() : string;
+    public abstract function getId() : string;
+    public abstract function getDescription() : string;
+    public function getHelpUrl() : string
+    {
+        return '';
+    }
+}
