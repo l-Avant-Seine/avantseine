6198b4fed9adbd4c8398b37763ea20ac5ab267df
Thomas <hello@thomasflorentin.net>
2026-04-01 17:47:34 +0200

    polish

--- wp-content/themes/lavantseine-v4/Components/blocs/bloc-newsletter.php
+++ wp-content/themes/lavantseine-v4/Components/blocs/bloc-newsletter.php
@@ -1,6 +1,8 @@
 <!-- AvantSeine Signup Form HTML Code -->
 <div class="bloc-newsletter">
 
+    <h3 class="h3">Recevez les dernières actualités de l’Avant Seine !</h3>
+
     <iframe data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://1hj9.mj.am/wgt/1hj9/mqz/form?c=2440e869" width="100%" style="height: 0;"></iframe>
 
     <script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script> 
---
+++ wp-content/themes/lavantseine-v4/Components/blocs/bloc-reseaux.php
@@ -0,0 +1,29 @@
+
+<!-- AvantSeine Signup Form HTML Code -->
+<div class="bloc-reseaux">
+
+    <h3 class="h3">Suivez-nous sur les réseaux !</h3>
+
+    <div classs="">
+        <ul class="no-bullets">
+            <li><a href="<?php the_field('link_facebook', 'option'); ?>" target="_blank">
+                <?php get_template_part('Components/svgs/svg', 'rs'); ?>
+            </a></li>
+            <li><a href="<?php the_field('link_instagram', 'option'); ?>" target="_blank">
+                <?php get_template_part('Components/svgs/svg', 'rs'); ?>
+            </a></li>
+            <li><a href="<?php the_field('link_youtube', 'option'); ?>" target="_blank">
+                <?php get_template_part('Components/svgs/svg', 'rs'); ?>
+            </a></li>
+            <li><a href="<?php the_field('link_linkedin', 'option'); ?>" target="_blank">
+                <?php get_template_part('Components/svgs/svg', 'rs'); ?>
+            </a></li>
+            <li><a href="<?php the_field('link_tiktok', 'option'); ?>" target="_blank">
+                <?php get_template_part('Components/svgs/svg', 'rs'); ?>
+            </a></li>
+        </ul>
+    </div>
+</div>
+
+
+
