1f200a2c7621e64046ed51f1eb934fcdfb5aa428
Thomas <hello@thomasflorentin.net>
2026-04-17 13:15:33 +0200

    polish

--- wp-content/themes/lavantseine-v4/Components/modules/module-ticker.php
+++ wp-content/themes/lavantseine-v4/Components/modules/module-ticker.php
@@ -1,5 +1,5 @@
 
-						<?php $words = $args['words']; ?>
+	<?php $words = $args['words']; ?>
 
 	<div class="webmag-ticker ticker-wrap">
 		<div class="ticker" style="animation-duration: <?php echo get_field('thicker_speed', 'option') . 's'; ?>">
@@ -8,13 +8,16 @@
 				if ($i % 2 === 0) { ?>
 					<span class="ticker__item">
 						<?php foreach( $words as $word ) : ?>
-							<span><?php echo $word; ?></span>
+							<p class="word"><?php echo $word; ?></p>
+							<span class="sep">&#8226;</span>
+
 						<?php endforeach; ?>
 					</span>
 				<?php } else { ?>
-					<span class="ticker__item red">
+					<span class="ticker__item">
 						<?php foreach( $words as $word ) : ?>
-							<span><?php echo $word; ?></span>
+							<p class="word"><?php echo $word; ?></p>
+							<span class="sep">&#8226;</span>
 						<?php endforeach; ?>
 					</span>
 			<?php }
