b2b00714c0dcaa01a07c4f4c5dc12dfa3bc0efe6
Thomas <hello@thomasflorentin.net>
2026-04-29 11:43:07 +0200

    test

--- wp-content/themes/lavantseine-v4/functions.php
+++ wp-content/themes/lavantseine-v4/functions.php
@@ -222,41 +222,23 @@ function get_events() {
 	
 	
 	// FORMAT AND SORT DATES ARRAY
-	if( is_array($posts) ) : foreach ( $posts as $post ) :
+	foreach ( $posts as $post ) :
 
-		$event_first_date = get_field( 'eventDetail_first_date', $post->ID );
+		$event_first_date = get_field( 'eventDetail_first_date', $post->ID ); 
 		$event_last_date = get_field( 'eventDetail_last_date', $post->ID );
 		$event_other_dates = get_field('eventDetail_otherdates', $post->ID);
 
 		if( $event_first_date ) { 
-
 			$d = new DateTime( date('m/d/Y', $event_first_date) );
 			$d->modify('midnight'); 
 			$d_ts = strtotime($d->format('Y-m-d H:i:s'));
 			$dates[ $event_first_date ] = array( 'id' => $post->ID, 'day_ts' => strval($d_ts));
 		}
 
-		if( $event_other_dates ) {
-			foreach( $event_other_dates as $o) {
-				if( isset($o['date'])) {
-					$d = new DateTime( date('m/d/Y', $o['date'] ) );
-					$d->modify('midnight'); 
-					$d_ts = strtotime($d->format('Y-m-d H:i:s'));
-					$dates[ $o['date']  ] = array( 'id' => $post->ID, 'day_ts' => strval($d_ts) );
-				}
-			}
-		}
 
-		if( $event_last_date !== $event_first_date ) {
-			if($event_last_date) {
-				$d = new DateTime( date('m/d/Y', $event_last_date) );
-				$d->modify('midnight'); 
-				$d_ts = strtotime($d->format('Y-m-d H:i:s'));
-				$dates[ $event_last_date ] = array( 'id' => $post->ID, 'day_ts' => strval($d_ts) );		
-			}
-		}
 
-	 endforeach; endif; wp_reset_query(); ksort($dates); //var_dump($dates); ?>
+	endforeach; wp_reset_query(); ksort($dates); //var_dump($dates); ?>
+
 
 	<?php 
 	    $content = ob_get_clean();
