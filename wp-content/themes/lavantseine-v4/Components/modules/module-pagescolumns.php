6d3a3b689f488b89b5216b344b104a259d38ff28
Thomas <hello@thomasflorentin.net>
2026-05-21 17:31:54 +0200

    4cols: Polish

--- wp-content/themes/lavantseine-v4/Components/modules/module-pagescolumns.php
+++ wp-content/themes/lavantseine-v4/Components/modules/module-pagescolumns.php
@@ -16,17 +16,22 @@
             <?php endif; ?>
         </div>
         
-        <div class="grid">
+        <div class="flex --justf">
             <?php foreach ( $posts as $key => $post) : setup_postdata( $post ); ?>
 
                 <div class="s_6col m_3col">
 
-                    <img class="mod_icon mb-small" src="<?php echo get_template_directory_uri(); ?>/assets/img/demilunes/demilune<?php echo $key+1; ?>.png">
+                    <a href="<?php the_permalink(); ?>" class="flex --jstf --col">
 
-                    <a href="<?php the_permalink(); ?>" class="--block">
-                        <h2 class="mod_title h2_2 mb-small"><?php the_title(); ?></h2>
-                        <div class="mod_excerpt mb-small"><?php the_field('pageDetail_intro'); ?></div>
-                        <div class="mod_cta"><?php get_template_part('Components/svgs/svg', 'arrow'); ?></div>
+                        <div>
+                            <img class="mod_icon mb-small" src="<?php echo get_template_directory_uri(); ?>/assets/img/demilunes/demilune<?php echo $key+1; ?>.png">
+
+                            <h2 class="mod_title h2_2 mb-small"><?php the_title(); ?></h2>
+                            <div class="mod_excerpt mb-small"><?php the_field('pageDetail_intro'); ?></div>
+                        </div>
+                        <div>
+                            <div class="mod_cta"><?php get_template_part('Components/svgs/svg', 'arrow'); ?></div>
+                        </div>
                     </a>
 
                 </div>
