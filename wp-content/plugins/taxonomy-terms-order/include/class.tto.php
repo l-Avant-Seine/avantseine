a9d02aa26b0d3704d46eae38ea1847467628aeb7
Thomas <hello@thomasflorentin.net>
2026-05-28 13:43:48 +0200

    updates: Category Order and Taxonomy Terms Order

--- wp-content/plugins/taxonomy-terms-order/include/class.tto.php
+++ wp-content/plugins/taxonomy-terms-order/include/class.tto.php
@@ -12,8 +12,8 @@
             */
             function __construct() 
                 {
-                    add_action('admin_print_scripts',               array ( $this,  'admin_print_scripts' ) );
-                    add_action('admin_print_styles',                array ( $this,  'admin_print_styles' ) );
+                    add_action('admin_enqueue_scripts',             array ( $this,  'admin_enqueue_scripts' ) );
+                    
                     add_action('admin_menu',                        array ( $this,  'admin_menu' ), 99);
                     add_filter('terms_clauses',                     array ( $this,  'apply_order_filter' ), 10, 3);
                     add_filter('get_terms_orderby',                 array ( $this,  'get_terms_orderby' ), 1, 2);
@@ -26,29 +26,26 @@
                     if ( is_admin() )
                         TTO_functions::check_table_column();                    
                 }
-                
+            
             
             /**
-            * Admin Scripts
-            *     
+            * Enqueue the styles nd scripts assets
+            * 
+            * @param mixed $hook
+            * @return mixed
             */
-            function admin_print_scripts()
+            function admin_enqueue_scripts( $hook )
                 {
+                    if ( strpos ( $hook, 'to-interface' )   === FALSE   &&  strpos ( $hook, 'settings_page_to-options' )   === FALSE )
+                        return;
+                        
+                    wp_register_style('to.css', TOURL . '/css/to.css', array(), TTO_VERSION );
+                    wp_enqueue_style( 'to.css');
+                    
                     wp_enqueue_script('jquery');                    
                     wp_enqueue_script('jquery-ui-sortable');
                     wp_register_script('to-javascript', TOURL . '/js/to-javascript.js', array(), TTO_VERSION, FALSE );
                     wp_enqueue_script( 'to-javascript');
-                }
-                
-            
-            /**
-            * Admin styles
-            * 
-            */
-            function admin_print_styles()
-                {
-                    wp_register_style('to.css', TOURL . '/css/to.css', array(), TTO_VERSION );
-                    wp_enqueue_style( 'to.css');
                 }
                 
                 
@@ -126,6 +123,8 @@
                     
                     $options = TTO_functions::get_settings();
                     
+                    $ignore_term_order = $args['ignore_term_order'] ?? false;
+                    
                     //if admin make sure use the admin setting
                     if (is_admin())
                         {
@@ -135,7 +134,7 @@
                             if (isset($_GET['orderby']) && sanitize_text_field( wp_unslash( $_GET['orderby'] ) ) !==  'term_order')
                                 return $clauses;
                             
-                            if ( $options['adminsort'] == "1" &&  (!isset($args['ignore_term_order']) ||  (isset($args['ignore_term_order'])  &&  $args['ignore_term_order']  !== TRUE) ) )
+                            if ($options['adminsort'] === "1" && $ignore_term_order !== true)
                                 {
                                     if ( $clauses['orderby']    ==  'ORDER BY t.name' )
                                         $clauses['orderby'] =   'ORDER BY t.term_order '. $clauses['order'] .', t.name';
@@ -148,14 +147,26 @@
                         }
                     
                     //if autosort, then force the menu_order
-                    if ($options['autosort'] == "1"   &&  (!isset($args['ignore_term_order']) ||  (isset($args['ignore_term_order'])  &&  $args['ignore_term_order']  !== TRUE) ) )
+                    if ($options['autosort'] === "1" && $ignore_term_order !== true ) 
                         {
                             $clauses['orderby'] =   'ORDER BY t.term_order';
+                            
+                            return $clauses;
                         }
+                    
+                    $rest_route = $GLOBALS['wp']->query_vars['rest_route'] ?? '';
+
+                    // Admin/dashboard REST calls typically use these namespaces
+                    $is_admin_rest = (
+                                        strpos( $rest_route, '/wp/v2/' ) === 0 ||   // core WP admin REST
+                                        strpos( $rest_route, '/wp-block' ) === 0      // block editor
+                                    );
                         
-                    if ( $options['adminsort'] == "1"   &&  defined( 'REST_REQUEST' ) && REST_REQUEST )
+                    if ( $is_admin_rest &&  $options['adminsort'] === "1"   &&  defined( 'REST_REQUEST' ) && REST_REQUEST )
                         {
-                            $clauses['orderby'] =   'ORDER BY t.term_order'; 
+                            $clauses['orderby'] =   'ORDER BY t.term_order';
+                            
+                            return $clauses; 
                         } 
                         
                     return $clauses; 
